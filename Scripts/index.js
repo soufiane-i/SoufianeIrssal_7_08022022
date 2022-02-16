e266a03856d8186291f482b613c63f56c69f922f
Soufiane Irssal <soufiane.irssal@gmail.com>
2022-02-16 12:40:31 +0100

    initialisation js

---
+++ Scripts/index.js
@@ -0,0 +1,12 @@
+console.log(recipes);
+
+function displayRecipes() {
+    //cards location
+	const cardsSection = document.querySelector('#cardsSection')
+
+    //cards creation loop
+}
+
+displayRecipes()
+
+
