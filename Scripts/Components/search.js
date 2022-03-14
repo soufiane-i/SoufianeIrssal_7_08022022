75b713e2c5db6c3365ce242e7f9caf6bd6034d56
Soufiane Irssal <soufiane.irssal@gmail.com>
2022-03-14 17:20:19 +0100

    master upadate

---
+++ Scripts/Components/search.js
@@ -0,0 +1,91 @@
+
+//Get SearchBar Input
+let searchBarRecipe
+let searchBarInput = document.querySelector('.form-control')
+
+searchBarInput.value = ''
+
+
+searchBarInput.addEventListener('input', characterCheck)
+
+const noRecipeFoundMsg = document.querySelector('.noRecipeFound')
+
+//If at least 3 character in serachBar call filter function, else do nothing
+function characterCheck()
+{
+    tagsSelected = document.querySelectorAll('.tag-selected')
+    if(searchBarInput.value.length >= 3) {
+        searchBarFilter()
+    } else {
+        if (tagsSelected.length > 0) filterByTag()
+        else displayRecipes(recipes)
+    }
+}
+
+//Filter for search Bar : title, decription and ingredients
+function searchBarFilter()
+{
+    //Get search bar input value
+    const userSearch = searchBarInput.value.toLowerCase()
+    //Clear Cards Section
+    cardsSection.innerHTML = ''
+    tagsSelected = document.querySelectorAll('.tag-selected')
+
+    if(tagsSelected.length > 0) {
+        filterByTag()
+    } else {
+        filterBySearchBar(userSearch)
+    
+        results = newResults
+        results.sort((a, b) => (a.name > b.name) ? 1 : -1) 
+    }
+
+    if (results.length == 0) {
+        noRecipeFoundMsg.classList.remove('close')
+    } else {
+        noRecipeFoundMsg.classList.add('close')
+        displayRecipes(results)
+    }
+    tagsAvailable(results)
+}
+
+//Filter ingredients in a recipeArray to define in parameter
+function ingredientsList(recipe, tag)
+{
+    if (recipe.ingredients.find(object => object.ingredient.includes(tag) && object.ingredient.length == tag.length)) return true
+    return false
+}
+
+function refreshTags() {
+    if (results.length == 0) {
+        ingredientsTags = recipes.map(recipe => recipe.ingredients.map(ingredient => ingredient.ingredient))
+        appliancesTags = recipes.map(recipe => recipe.appliance)
+        ustensilsTags = recipes.map(recipe => recipe.ustensils)
+    } else {
+        ingredientsTags = results.map(recipe => recipe.ingredients.map(ingredient => ingredient.ingredient))
+        appliancesTags = results.map(recipe => recipe.appliance)
+        ustensilsTags = results.map(recipe => recipe.ustensils)
+    }
+}
+
+function filterBySearchBar(userSearch) {
+   //Algo Native Loop
+   let newResultsWithDuplicate = []
+   newResults = []
+
+   for (let i = 0; i < recipes.length; i++) {
+    if (recipes[i].name.toLowerCase().includes(userSearch)) {
+        newResultsWithDuplicate.push(recipes[i])
+    }
+    if (recipes[i].description.toLowerCase().includes(userSearch)) {
+        newResultsWithDuplicate.push(recipes[i])
+    } 
+    if (recipes[i].ingredients.find(object => object.ingredient.toLowerCase().includes(userSearch))) {
+        newResultsWithDuplicate.push(recipes[i])
+    }   
+}
+
+   newResults = [...new Set(newResultsWithDuplicate)]
+   console.log(newResults);
+  
+}
